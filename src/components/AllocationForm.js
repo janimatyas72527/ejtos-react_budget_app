b6ef816b261aaf30b4a609639896c4f223f1b7bd
Matyas Jani <jani.matyas@gmail.com>
2023-06-03 15:27:15 -0400

    Editable budget

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -58,13 +58,13 @@ const AllocationForm = (props) => {
                         <option defaultValue value="Add" name="Add">Add</option>
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
-
+                    <label for="#cost" style={{marginLeft: '2rem'}}>£</label>
                     <input
                         required='required'
                         type='number'
                         id='cost'
                         value={cost}
-                        style={{ marginLeft: '2rem' , size: 10}}
+                        style={{size: 10}}
                         onChange={(event) => setCost(event.target.value)}>
                         </input>
 
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,27 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
+
+const dispatchIfCorrect = (dispatch, remaining, event) => {
+    if (event.target.value > 20000) {
+        window.alert("Budget cannot exceed £20000");
+    } else if (remaining < 0) {
+        window.alert("Budget cannot be lower than the spending!");
+    } else {
+        dispatch({type: "SET_BUDGET", payload: event.target.value });
+    }
+} 
+
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, remaining, dispatch } = useContext(AppContext);
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £</span>
+            <input type="number"
+                step="10"
+                min="0"
+                value={budget}
+                onChange={event => dispatchIfCorrect(dispatch, remaining, event)}
+                />
         </div>
     );
 };
